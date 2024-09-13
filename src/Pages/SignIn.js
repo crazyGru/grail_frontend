897510f9ba0e743230882462375af38847c4adcf
crazyGru <alimanson0412@gmail.com>
2024-09-13 16:43:57 -0500

    complete/signin

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -3,23 +3,46 @@ import "./SignIn.css";
 import tl from "../top_left.png";
 import br from "../bottom_right.png";
 import logo from "../logo_.png";
-
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagramSquare } from "react-icons/fa";
-
+import { useDispatch } from "react-redux";
 import { signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "../utilis/config";
+import { setUser } from "../redux/userSlice";
 
 const SignIn = () => {
+  const dispatch = useDispatch();
+
   const handleGoogleSignIn = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
-      console.log(result.user);
+      const user = result.user;
+      const userName = encodeURIComponent(user.displayName);
+      const userEmail = encodeURIComponent(user.email);
+      const userProfilePic = encodeURIComponent(user.photoURL);
+
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}/users/google-signin?name=${userName}&email=${userEmail}&picture=${userProfilePic}`,
+        {
+          method: "POST",
+          headers: {
+            accept: "application/json",
+          },
+        }
+      );
+
+      const data = await response.json();
+      if (response.ok) {
+        dispatch(setUser(data));
+      } else {
+        console.error(data.detail);
+      }
     } catch (error) {
       console.error(error);
     }
   };
+
   return (
     <div className="page-signin">
       <img src={tl} className="tl_logo" alt="Top Left" />
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import AppWrapper from "./App";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppWrapper />
+    <Provider store={store}>
+      <AppWrapper />
+    </Provider>
   </React.StrictMode>
 );
 
