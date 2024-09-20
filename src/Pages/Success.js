f350314fead74bf6a329913ef887ffdfde68ae2d
crazyGru <alimanson0412@gmail.com>
2024-09-19 21:00:33 -0500

    complete/beta

--- src/Pages/Success.js
+++ src/Pages/Success.js
@@ -4,19 +4,66 @@ import Logo from "../logo_.png";
 import { useState } from "react";
 import { LuCopy } from "react-icons/lu";
 import { LuCopyCheck } from "react-icons/lu";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { FaEye } from "react-icons/fa";
 
 const Success = () => {
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user.user);
   const [copied, setCopied] = useState(false);
-  const licenseKey =
-    "Z0FBQUFBQm03S0F2dzd5RnZReElLUDJyUmZvNENtaHBtWmJUQl9WU0Z0VjBheFk4WHF2VEJfMy13WHM1WHV2R25acnpQcnlxWGM4eEVkY0Y1d2ZNdEpLeWVTUzFfb3FCS1NSRlZKWDQ2MkxnanZMSFZISHBlcjA3M0tzX1cyZEk4elVrRnpyNnJFbXlSS0JSZ0lWSTczT2htVkZzR1RuaktQNnZvOFBJZk1JSlVPSVp3WHdUbzJpR2R5Y1ROckpRNjNSTjdpTWcyeWxYYUwyYnlLVk1STXFSY296OTJGMkZGOG1tNE9LVkxaaHdHZzI2NktqM2lIUG9RdXpkY1lSODNWT2s3Rk14YnJmUGpMaWhubHhsWG1xck9WRG9LZjJLaEE9PQ==";
+  const [licenses, setLicenses] = useState([]);
+  const [shortenedKey, setShortenedKey] = useState(
+    "**************************************************************"
+  );
+
+  const fetchLicenses = async () => {
+    if (!user) {
+      navigate("/signin");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `${
+          process.env.REACT_APP_BACKEND_URL
+        }/subscriptions/available-licenses?user_email=${encodeURIComponent(
+          user
+        )}`,
+        {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch licenses");
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setLicenses(data);
+      setShortenedKey(
+        `${data[0].license_key.slice(0, 30)}...${data[0].license_key.slice(
+          -30
+        )}`
+      );
+    } catch (error) {
+      console.error("Error fetching licenses:", error);
+    }
+  };
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(licenseKey);
+    navigator.clipboard.writeText(licenses[0].license_key);
     setCopied(true);
     setTimeout(() => setCopied(false), 2000); // Reset copied state after 2 seconds
   };
 
-  const shortenedKey = `${licenseKey.slice(0, 30)}...${licenseKey.slice(-30)}`;
+  const handleBackToHome = () => {
+    navigate("/"); // Redirect to home
+  };
 
   return (
     <div className="success">
@@ -29,9 +76,14 @@ const Success = () => {
             <button className="copy-button" onClick={handleCopy}>
               {copied ? <LuCopyCheck /> : <LuCopy />}
             </button>
+            <button className="copy-button" onClick={fetchLicenses}>
+              <FaEye />
+            </button>
           </span>
         </div>
-        <div className="back-message">Back to home</div>
+        <div className="back-message" onClick={handleBackToHome}>
+          Back to home
+        </div>
       </div>
     </div>
   );
