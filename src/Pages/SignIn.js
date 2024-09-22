8e84371bc4b91376ed00031212fdf334c23a97cd
crazyGru <alimanson0412@gmail.com>
2024-09-22 10:42:58 -0500

    reset/fix

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -4,8 +4,6 @@ import tl from "../top_left.png";
 import br from "../bottom_right.png";
 import logo from "../logo_.png";
 import { FaGoogle } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagramSquare } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "../utilis/config";
@@ -64,14 +62,6 @@ const SignIn = () => {
         <div className="sign-button" onClick={handleGoogleSignIn}>
           <FaGoogle style={{ marginRight: "8px" }} />{" "}
           <span>Continue with Google</span>
-        </div>
-        <div className="sign-button">
-          <FaFacebook style={{ marginRight: "8px" }} />{" "}
-          <span>Continue with Facebook</span>
-        </div>
-        <div className="sign-button">
-          <FaInstagramSquare style={{ marginRight: "8px" }} />{" "}
-          <span>Continue with Instagram</span>
         </div>
       </div>
     </div>
